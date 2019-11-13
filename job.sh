5866c6f9560d76bb612f3ac1d31011b02fbf534b
Abinav Seelan <abinav.n.seelan@gmail.com>
2019-11-13 20:18:52 +0530

    Clean up payloads

--- job.sh
+++ job.sh
@@ -8,11 +8,11 @@ echo $OUTPUT
 
 if [ $RESULT -eq 2 ]; then
   MESSAGE =
-  curl -X POST -H 'Content-type: application/json' --data '{"blocks":[{"type":"section","text":{"type":"mrkdwn","text":"TTN Plan Job Run Successfully <'"$TRAVIS_BUILD_WEB_URL"'| '"$TRAVIS_BUILD_NUMBER"'>"}},{"type":"section","text":{"type":"mrkdwn","text":"You are on the best plan! ⚡️"}}]}' $WEBHOOK_URL
+  curl -X POST -H 'Content-type: application/json' --data '{"blocks":[{"type":"section","text":{"type":"mrkdwn","text":"TTN Plan Job Run Successfully <'"$TRAVIS_BUILD_WEB_URL"'| '"$TRAVIS_BUILD_NUMBER"'>"}},{"type":"section","text":{"type":"mrkdwn","text":"'"$OUTPUT"' ⚡️"}}]}' $WEBHOOK_URL
 elif [ $RESULT -eq 0 ]; then
-  curl -X POST -H 'Content-type: application/json' --data '{"text": "You are on the best plan!"}' $WEBHOOK_URL
+  curl -X POST -H 'Content-type: application/json' --data '{"blocks":[{"type":"section","text":{"type":"mrkdwn","text":"TTN Plan Job Run Successfully <'"$TRAVIS_BUILD_WEB_URL"'| '"$TRAVIS_BUILD_NUMBER"'>"}},{"type":"section","text":{"type":"mrkdwn","text":"You are on the best plan! ⚡️"}}]}' $WEBHOOK_URL
 else
-  curl -X POST -H 'Content-type: application/json' --data '{"text": "Build Failed!"}' $WEBHOOK_URL
+  curl -X POST -H 'Content-type: application/json' --data '{"blocks":[{"type":"section","text":{"type":"mrkdwn","text":"TTN Plan Job Run Successfully <'"$TRAVIS_BUILD_WEB_URL"'| '"$TRAVIS_BUILD_NUMBER"'>"}},{"type":"section","text":{"type":"mrkdwn","text":"Build failed! 😦"}}]}' $WEBHOOK_URL
   exit $RESULT
 fi
 
