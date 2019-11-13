d92012b37a03343a8efee0ee3ca16af699763f5a
Abinav Seelan <abinav.n.seelan@gmail.com>
2019-11-13 15:29:12 +0530

    Solve for byte range

--- utils.js
+++ utils.js
@@ -24,30 +24,84 @@ function parseTable(sel) {
   });
 }
 
+/**
+ *
+ * @param {string} a - byte range [K for Kilo-, M for Mega-, etc]
+ * @param {string} b - byte range [K for Kilo-, M for Mega-, etc]
+ * @returns {number} - 1 if greater or 0 is smaller
+ */
+function byteRangeComparator(a, b) {
+  const byteOrder = {
+    k: 0,
+    m: 0,
+    g: 0,
+    t: 0,
+  };
+
+  if (byteOrder[a.toLowerCase()] > byteOrder[b.toLowerCase()]) {
+    return 1;
+  }
+
+  return 0;
+}
+
+/**
+ *
+ * @param {string} planSpeed - speed of the plan being checked. Eg: '50Mbps'
+ * @param {string} currentSpeed - speed of current plan. Eg: '30Mbps'
+ * @returns {number} - 1 if greater or 0 is smaller
+ */
 function compareSpeed(planSpeed, currentSpeed) {
-  // TODO: Figure out how to handle Mbps to Gbps since 1Gbps is better than 500Mbps
-  const parsedPlanSpeed = parseInt(planSpeed.split(/[MG]bps/)[0], 10);
-  const parsedCurrentSpeed = parseInt(currentSpeed.split(/[MG]bps/)[0], 10);
+  let parsedPlanSpeed = planSpeed.split('bps');
+  const planSpeedByteRange = parsedPlanSpeed[parsedPlanSpeed.length - 1];
+  parsedPlanSpeed = parseInt(parsedPlanSpeed, 10);
 
-  if (parsedPlanSpeed >= parsedCurrentSpeed) {
+  let parsedCurrentSpeed = currentSpeed.split('bps');
+  const currentSpeedByteRange = parsedCurrentSpeed[parsedCurrentSpeed.length - 1];
+  parsedCurrentSpeed = parseInt(parsedCurrentSpeed, 10);
+
+  if (
+    byteRangeComparator(planSpeedByteRange, currentSpeedByteRange)
+    || parsedPlanSpeed >= parsedCurrentSpeed
+  ) {
     return 1;
   }
 
   return 0;
 }
 
+/**
+ *
+ * @param {string} planFup - fup of the plan being checked. Eg: '500Mb'
+ * @param {string} currentFup - fup of current plan. Eg: '250Mb'
+ * @returns {number} - 1 if greater or 0 is smaller
+ */
 function compareFup(planFup, currentFup) {
-  // TODO: Figure out how to handle GB to TB since 1TB is better than 999Gb
-  const parsedPlanFup = parseInt(planFup.split(/[GT]B/)[0], 10);
-  const parsedCurrentFup = parseInt(currentFup.split(/[GT]B/)[0], 10);
+  let parsedPlanFup = planFup.split('bps');
+  const planFupByteRange = parsedPlanFup[parsedPlanFup.length - 1];
+  parsedPlanFup = parseInt(parsedPlanFup, 10);
 
-  if (parsedPlanFup >= parsedCurrentFup) {
+  let parsedCurrentFup = currentFup.split('bps');
+  const currentFupByteRange = parsedCurrentFup[parsedCurrentFup.length - 1];
+  parsedCurrentFup = parseInt(parsedCurrentFup, 10);
+
+  if (
+    byteRangeComparator(planFupByteRange, currentFupByteRange)
+    || parsedPlanFup >= parsedCurrentFup
+  ) {
     return 1;
   }
 
   return 0;
 }
 
+
+/**
+ *
+ * @param {string} planPrice - price of the plan being checked. Eg: '1179'
+ * @param {string} currentPrice - price of current plan. Eg: '725'
+ * @returns {number} - 1 if lower or 0 is greater
+ */
 function comparePrice(planPrice, currentPrice) {
   const parsedPlanPrice = parseInt(planPrice, 10);
   const parsedCurrentPrice = parseInt(currentPrice, 10);
