d3b9656be7d620ae9609e4b5e760b54e56c4eadc
Aditi Mohanty <rheaditi@users.noreply.github.com>
2019-11-17 20:19:04 +0530

    Fix warnings

--- job.sh
+++ job.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# run this file through `shellcheck` and/or `shfmt` when editing to ensure there are no warnings/issues.
+
 set -eux # exit as soon as any line in the script fails
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
@@ -10,15 +12,15 @@ else
   OUTPUT="$(npm run start --silent)"
   RESULT=$?
 
-  echo $RESULT
-  echo $OUTPUT
+  echo "$RESULT"
+  echo "$OUTPUT"
 
   if [ $RESULT -eq 2 ]; then
-    curl -X POST -H 'Content-type: application/json' --data '{"blocks":[{"type":"section","text":{"type":"mrkdwn","text":"Job run successfully!\n*<'"$TRAVIS_BUILD_WEB_URL"'| #'"$TRAVIS_BUILD_NUMBER"'>*"}},{"type":"section","text":{"type":"mrkdwn","text":"*'"$OUTPUT"' 🔺*"}}]}' $WEBHOOK_URL
+    curl -X POST -H 'Content-type: application/json' --data '{"blocks":[{"type":"section","text":{"type":"mrkdwn","text":"Job run successfully!\n*<'"$TRAVIS_BUILD_WEB_URL"'| #'"$TRAVIS_BUILD_NUMBER"'>*"}},{"type":"section","text":{"type":"mrkdwn","text":"*'"$OUTPUT"' 🔺*"}}]}' "$WEBHOOK_URL"
   elif [ $RESULT -eq 0 ]; then
-    curl -X POST -H 'Content-type: application/json' --data '{"blocks":[{"type":"section","text":{"type":"mrkdwn","text":"Job run successfully!\n*<'"$TRAVIS_BUILD_WEB_URL"'| #'"$TRAVIS_BUILD_NUMBER"'>*"}},{"type":"section","text":{"type":"mrkdwn","text":"You are on the best plan! ⚡️"}}]}' $WEBHOOK_URL
+    curl -X POST -H 'Content-type: application/json' --data '{"blocks":[{"type":"section","text":{"type":"mrkdwn","text":"Job run successfully!\n*<'"$TRAVIS_BUILD_WEB_URL"'| #'"$TRAVIS_BUILD_NUMBER"'>*"}},{"type":"section","text":{"type":"mrkdwn","text":"You are on the best plan! ⚡️"}}]}' "$WEBHOOK_URL"
   else
-    curl -X POST -H 'Content-type: application/json' --data '{"blocks":[{"type":"section","text":{"type":"mrkdwn","text":"Job run successfully!\n*<'"$TRAVIS_BUILD_WEB_URL"'| #'"$TRAVIS_BUILD_NUMBER"'>*"}},{"type":"section","text":{"type":"mrkdwn","text":"Build failed! 😦"}}]}' $WEBHOOK_URL
+    curl -X POST -H 'Content-type: application/json' --data '{"blocks":[{"type":"section","text":{"type":"mrkdwn","text":"Job run successfully!\n*<'"$TRAVIS_BUILD_WEB_URL"'| #'"$TRAVIS_BUILD_NUMBER"'>*"}},{"type":"section","text":{"type":"mrkdwn","text":"Build failed! 😦"}}]}' "$WEBHOOK_URL"
     exit $RESULT
   fi
 fi;
